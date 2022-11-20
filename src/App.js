aa9abccc2e4ed7d6838f94242bded0dee9aca495
zehir <fcanmekikoglu@gmail.com>
2022-11-20 15:28:29 +0300

    create post

--- src/App.js
+++ src/App.js
@@ -22,12 +22,19 @@ function App() {
   return <Router>
   <nav>
     <Link to="/"> Home </Link>
-    <Link to="/createpost"> Create Post </Link>
-    {!isAuth ? <Link to="/login"> Login </Link> : <button onClick={signUserOut}> Log Out</button>}
+    
+    {!isAuth ? (
+      <Link to="/login"> Login </Link>
+      ) : (
+        <>
+        <Link to="/createpost"> Create Post </Link>
+        <button onClick={signUserOut}> Log Out</button>
+        </>
+        )}
   </nav>
   <Routes>
     <Route path='/' element={<Home/>}></Route>
-    <Route path='/createpost' element={<CreatePost/>}></Route>
+    <Route path='/createpost' element={<CreatePost isAuth={isAuth}/>}></Route>
     <Route path='/login' element={<Login setIsAuth={setIsAuth}/>}></Route>
   </Routes>
   </Router>
